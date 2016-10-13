134cfd2afc63052ae5bb24f32915847add8f02ed
Fred.liu <liuzx.soho@gmail.com>
2016-10-13 17:33:45 +0800

    Accelerate the development Speed

---
+++ mock.bat
@@ -0,0 +1,9 @@
+@echo on
+::rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+::rem set PATH=%JAVA_HOME%/bin
+::rem set CLASSPATH=%JAVA_HOME%/lib/tools.jar;
+::rem javac TestDate.java
+::rem java TestDate.class
+::rem path=D:\Android_Software\Mock
+java -jar wiremock-1.46-standalone.jar --port 9991
+@echo off
